dee27b2fac2b6159d5b0763a86988a891f42b622
Iván Trapero <regenzel@gmail.com>
2025-12-11 23:36:45 +0100

    Añado un nuevo ejercicio

---
+++ concatenar_con_comillas.py
@@ -0,0 +1,8 @@
+def quote_and_author():
+    quote = input("Inserte la cita celebre: ")
+    author = input("Inserte su correspondiente autor: ")
+    final_quote = '"'
+    print(final_quote + quote + final_quote, "-" + author)
+
+quote_and_author()
+
