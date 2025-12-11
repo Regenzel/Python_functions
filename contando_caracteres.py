875959691f9b5c8376cc1f0b7ab37a90141fc16e
Iván Trapero <regenzel@gmail.com>
2025-12-11 22:45:36 +0100

    Modifico contando_caracteres para que sea mas simple

--- contando_caracteres.py
+++ contando_caracteres.py
@@ -1,6 +1,6 @@
 def count_chars():
     '''El usuario inserta una cadena y devuelve su longitud'''
     chars = input("Inserte una cadena de caracteres: ")
-    return print(chars + " = " + str(len(chars)) + " chars")
+    print(chars + " = " + str(len(chars)) + " chars")
 
 count_chars()
