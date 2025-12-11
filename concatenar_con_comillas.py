db8acb63bd4701b5eda129f5d00be07b279e4aeb
Iván Trapero <regenzel@gmail.com>
2025-12-11 23:37:51 +0100

    Añado docstring al ultimo ejercicio

--- concatenar_con_comillas.py
+++ concatenar_con_comillas.py
@@ -1,4 +1,6 @@
 def quote_and_author():
+    '''Pide al usuario que escriba una cita celebre y un autor y devuelve la cita 
+    entre comillas y el autor'''
     quote = input("Inserte la cita celebre: ")
     author = input("Inserte su correspondiente autor: ")
     final_quote = '"'
