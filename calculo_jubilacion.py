0290a8962415824bdcd592206f3c9714f28d4a20
Iván Martín Trapero <regenzel@gmail.com>
2025-12-18 23:13:47 +0100

    Añado primera version sin control de errores

---
+++ calculo_jubilacion.py
@@ -0,0 +1,14 @@
+from datetime import date
+
+def how_much_for_retirement():
+    year = input("¿Cuantos años tienes?: ")
+    when_you_retire = input ("¿A que edad te jubilaras?: ")
+    actual_year = date.today().year
+    y = int(when_you_retire) - int(year)
+    final_y = actual_year + int(when_you_retire)
+    if y < 0:
+       return print("Ya puede retirarse")
+
+    return print(f"- Te quedan {y} años para jubilarte.\n- Estamos en {actual_year}, te jubilarás en {final_y}.")
+
+how_much_for_retirement()
